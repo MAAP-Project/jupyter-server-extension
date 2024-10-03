fb12c5d72a82332b538c4d4caa7b54b2132f2348
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2024-10-03 10:35:05 -0700

    switched to using host parameter instead of environment variable

--- maap_jupyter_server_extension/handlers.py
+++ maap_jupyter_server_extension/handlers.py
@@ -28,7 +28,7 @@ def get_maap_config(host):
     # NOTE need to extract the path from the API 
     # What about a weird case where API host is sent or vice versa and not the other? 
     api_host = os.environ.get("MAAP_API_HOST") or "api.maap-project.org"
-    ade_host = os.environ.get("MAAP_ADE_HOST") or "ade.maap-project.org"
+    ade_host = host or "ade.maap-project.org"
     environments_endpoint = "https://" + api_host + "/api/environment/config/"+urllib.parse.quote(urllib.parse.quote("https://", safe=""))+ade_host
     print("graceal1 attempting to fetch")
     print(environments_endpoint)
