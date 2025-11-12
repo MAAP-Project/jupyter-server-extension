9a97400c3d30b2b5c69905ef68339f7fc68cabf4
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-11-12 15:56:30 -0800

    added print statement

--- maap_jupyter_server_extension/handlers.py
+++ maap_jupyter_server_extension/handlers.py
@@ -76,7 +76,9 @@ class WorkspaceContainerHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
+    #@tornado.web.authenticated
     def get(self):
+        print("graceal1 in WorkspaceContainerHandler")
         dockerimage_path_default = os.getenv('DOCKERIMAGE_PATH_DEFAULT')
         dockerimage_path_base_image = os.getenv("DOCKERIMAGE_PATH_BASE_IMAGE")
         self.finish({"DOCKERIMAGE_PATH_DEFAULT": dockerimage_path_default, "DOCKERIMAGE_PATH_BASE_IMAGE": dockerimage_path_base_image})
