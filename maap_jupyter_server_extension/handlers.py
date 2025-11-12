e1a31d72169cabe1f446cb284e06faac6e61b292
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-11-12 15:43:49 -0800

    got rid of tornado authentication tag for WorkspaceContainerHandler

--- maap_jupyter_server_extension/handlers.py
+++ maap_jupyter_server_extension/handlers.py
@@ -76,7 +76,6 @@ class WorkspaceContainerHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
-    @tornado.web.authenticated
     def get(self):
         dockerimage_path_default = os.getenv('DOCKERIMAGE_PATH_DEFAULT')
         dockerimage_path_base_image = os.getenv("DOCKERIMAGE_PATH_BASE_IMAGE")
