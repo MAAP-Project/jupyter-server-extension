a2f0892d7266047fb1502435d3f872e850861266
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2025-12-16 14:37:55 -0800

    cleaned server code | updated docs

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,56 @@ import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
 const MAAP_JUPYTER_SERVER_EXTENSION_ID = 'maap-jupyter-server-extension:plugin';
 
+interface IMaapParams {
+  maapApiUrl: string;
+  maapToken: string;
+}
+
 const plugin: JupyterFrontEndPlugin<void> = {
   id: MAAP_JUPYTER_SERVER_EXTENSION_ID,
-  description: 'MAAP Jupyter Server Extension',
   autoStart: true,
   requires: [ISettingRegistry],
   activate: async (app: JupyterFrontEnd, settings: ISettingRegistry) => {
-    const setting = await settings.load(MAAP_JUPYTER_SERVER_EXTENSION_ID);
+    // Load settings for this extension
+    const serverExtSettings = await settings.load(
+      MAAP_JUPYTER_SERVER_EXTENSION_ID
+    );
     const baseUrl = PageConfig.getBaseUrl();
 
-    // setting.changed.connect(() => {
-    //   const newUrl = setting.get('maapApiUrl').composite as string;
-    //   const newToken = setting.get('maapToken').composite as string;
-    //   console.log('MAAP API URL updated by user:', newUrl);
+    // Add listener to detect changes in extension settings
+    // serverExtSettings.changed.connect(() => {
+    //   const newUrl = serverExtSettings.get('maapApiUrl').composite as string;
+    //   const newToken = serverExtSettings.get('maapToken').composite as string;
+    //   console.log('MAAP API URL updated by user: ', newUrl, newToken);
     // });
 
-    // Get MAAP API URL and MAAP PGT Token and set them in the MAAP Extension settings
-    try {
-      const res = await fetch(
-        `${baseUrl}maap-jupyter-server-extension/get-maap-params`
-      );
-      const { token, apiUrl } = await res.json();
-      await setting.set('maapApiUrl', apiUrl);
-      await setting.set('maapToken', token);
-    } catch (error) {
-      console.error('Failed to fetch MAAP parameters from server:', error);
-    }
+    // Retrieve MAAP variables from environment and add them to the jupyter MAAP settings
+    fetch(`${baseUrl}maap-jupyter-server-extension/get-maap-params`)
+      .then(response => {
+        if (!response.ok) {
+          return response.text().then(data => {
+            return Promise.reject(
+              new Error(`${response.status} ${response.statusText}: ${data}`)
+            );
+          });
+        }
+        return response.json();
+      })
+      .then(async (maapParams: IMaapParams) => {
+        // Update extension settings with the fetched MAAP environment variables
+        try {
+          await Promise.all([
+            serverExtSettings.set('maapApiUrl', maapParams.maapApiUrl),
+            serverExtSettings.set('maapToken', maapParams.maapToken)
+          ]);
+          console.log('Successfully updated MAAP extension settings.');
+        } catch (error) {
+          console.error('Failed to update MAAP extension settings: ', error);
+        }
+      })
+      .catch(error => {
+        console.error('Failed to fetch MAAP parameters from server: ', error);
+      });
   }
 };
 
