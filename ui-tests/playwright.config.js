79221fbca2022441163d661440993a949af51647
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2025-12-19 09:03:24 -0800

    added linting

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -10,7 +10,7 @@ module.exports = {
     url: 'http://localhost:8888/lab',
     env: {
       MAAP_API_URL: 'https://api.dit.maap-project.org/api/',
-      MAAP_PGT_TOKEN: 'PGT-my_test_token',
+      MAAP_PGT_TOKEN: 'PGT-my_test_token'
     },
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI
