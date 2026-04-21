76249b744bcd3f4437d5395eb039179dfc6da65e
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2026-04-21 13:16:59 -0700

    removed unnecessary logic

--- maap_jupyter_server_extension/handlers.py
+++ maap_jupyter_server_extension/handlers.py
@@ -5,24 +5,7 @@ from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
 import subprocess
-import urllib.parse
-import maap_jupyter_server_extension.constants as constants
 from notebook.base.handlers import IPythonHandler
-from maap.maap import MAAP #dependency on maap-py but I am not sure workaround right now 
-import requests
-
-def get_maap_config(host):
-    api_host = os.getenv("MAAP_API_HOST", constants.DEFAULT_API)
-    maap_api_config_endpoint = os.getenv("MAAP_API_CONFIG_ENDPOINT", "api/environment/config")
-    ade_host = host if host in constants.ADE_OPTIONS else os.getenv("MAAP_ADE_HOST", constants.DEFAULT_ADE)
-    environments_endpoint = "https://" + api_host + "/" + maap_api_config_endpoint + "/"+urllib.parse.quote(urllib.parse.quote("https://", safe=""))+ade_host
-    return requests.get(environments_endpoint).json()
-
-def dps_bucket_name(host):
-	return get_maap_config(host)['workspace_bucket'] # Do I need WORKSPACE_BUCKET??
-
-def maap_api_url(host):
-	return 'https://{}'.format(get_maap_config(host)['api_server'])
 
 def is_valid_env_var_value(value: str) -> bool:
     """
@@ -52,11 +35,6 @@ def format_api_url(api_host: str) -> str:
 
     # Add trailing slash
     return api_host + '/'
-
-class MAAPConfigEnvironmentHandler(APIHandler):
-    def get(self):  
-        env = get_maap_config(self.request.host)
-        self.finish(env)
 
 class TestHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
@@ -184,7 +162,8 @@ class GetMaapParamsHandler(APIHandler):
                     "maapToken": "token_value" or "",
                     "maapApiUrl": "api_url_value" or "",
                     "defaultAppImage": "docker_image_path_default" or "",
-                    "currentAppImage": "docker_image_path_current" or ""
+                    "currentAppImage": "docker_image_path_current" or "",
+                    "workspaceBucket": "workspace_bucket" or ""
                 }
 
             500 Internal Server Error:
@@ -200,88 +179,19 @@ class GetMaapParamsHandler(APIHandler):
             api_url = format_api_url(api_host)
             docker_image_path_default = os.environ.get('DOCKERIMAGE_PATH_DEFAULT', "")
             docker_image_path_current = os.environ.get('DOCKERIMAGE_PATH_BASE_IMAGE', "")
+            workspace_bucket = os.environ.get("WORKSPACE_BUCKET", "")
             self.finish(json.dumps({
                     "maapToken": token,
                     "maapApiUrl": api_url,
                     "defaultAppImage": docker_image_path_default,
-                    "currentAppImage": docker_image_path_current
+                    "currentAppImage": docker_image_path_current,
+                    "workspaceBucket": workspace_bucket
                 }))
         except Exception as e:
             self.set_status(500)
             self.finish(json.dumps({
                 "error": str(e)
             }))
-
-class Presigneds3UrlHandler(IPythonHandler):
-
-    def get(self):
-        # get arguments
-        bucket = dps_bucket_name(self.request.host)
-        key = self.get_argument('key', '')
-        rt_path = os.path.expanduser(self.get_argument('home_path', ''))
-        abs_path = os.path.join(rt_path, key)
-        proxy_ticket = self.get_argument('proxy-ticket','')
-        expiration = self.get_argument('duration','86400') # default 24 hrs
-        # This is replacing the workspace name because the buckets correspond to the username
-        username = self.get_argument('username', '')
-
-        print('bucket is '+bucket)     
-        print('key is '+key)        
-        print('full path is '+abs_path) 
-
-        # -----------------------
-        # Checking for bad input
-        # -----------------------
-        # if directory, return error - dirs not supported
-        if os.path.isdir(abs_path):
-            self.finish({"status_code": 412, "message": "error", "url": "Presigned S3 links do not support folders"})
-            return
-
-        # check if file in valid folder (under mounted folder path)
-        resp = subprocess.check_output("df -h | grep s3fs | awk '{print $6}'", shell=True).decode('utf-8')
-        mounted_dirs = resp.strip().split('\n')
-        print(mounted_dirs)
-        if len(mounted_dirs) == 0:
-            self.finish({"status_code": 412, "message": "error",
-                "url": "Presigned S3 links can only be created for files in a mounted org or user folder" +
-                    "\nMounted folders include:\n{}".format(resp)
-                })
-            return
-
-        if not any([mounted_dir in abs_path for mounted_dir in mounted_dirs]):
-            self.finish({"status_code": 412, "message": "error",
-                "url": "Presigned S3 links can only be created for files in a mounted org or user folder" +
-                    "\nMounted folders include:\n{}".format(resp)
-                })
-            return
-
-        # -----------------------
-        # Generate S3 Link
-        # -----------------------
-        # if valid path, get presigned URL
-        # expiration = '43200' # 12 hrs in seconds
-        print('expiration is {} seconds', expiration)
-
-        url = '{}/api/members/self/presignedUrlS3/{}/{}?exp={}&ws={}'.format(maap_api_url(self.request.host), bucket, key, expiration, username)
-        headers = {'Accept': 'application/json', 'proxy-ticket': proxy_ticket}
-        r = requests.get(
-            url,
-            headers=headers,
-            verify=False
-        )
-        print(r.text)
-
-        resp = json.loads(r.text)   
-        self.finish({"status_code":200, "message": "success", "url":resp['url']})
-
-class AccountInfoPGTENVHandler(IPythonHandler):
-    @tornado.web.authenticated
-    def get(self):
-        proxy_granting_ticket = os.environ.get('MAAP_PGT', '')
-
-        maap = MAAP()
-        profile = maap.profile.account_info(proxy_ticket = proxy_granting_ticket)
-        self.finish({"profile": profile})
 
 
 def setup_handlers(web_app):
@@ -295,18 +205,12 @@ def setup_handlers(web_app):
     get_token_route = url_path_join(base_url, "maap-jupyter-server-extension", "get-token")
     get_maap_params_route = url_path_join(base_url, "maap-jupyter-server-extension", "get-maap-params")
     get_inject_public_key_route = url_path_join(base_url, "maap-jupyter-server-extension", "inject-public-key")
-    get_config_route = url_path_join(base_url, "maap-jupyter-server-extension", "get-config")
-    get_account_info_pgt_token_route = url_path_join(base_url, "maap-jupyter-server-extension", "get-account-into-pgt-token")
-    get_presigned_s3_url_route = url_path_join(base_url, "maap-jupyter-server-extension", "get-presigned-s3-url")
 
     handlers = [
         (test_route, TestHandler), 
         (get_api_url_route, GetApiUrlHandler),
         (get_token_route, GetTokenHandler),
         (get_maap_params_route, GetMaapParamsHandler),
-        {get_inject_public_key_route, InjectKeyHandler},
-        {get_config_route, MAAPConfigEnvironmentHandler},
-        {get_account_info_pgt_token_route, AccountInfoPGTENVHandler},
-        {get_presigned_s3_url_route, Presigneds3UrlHandler}
+        {get_inject_public_key_route, InjectKeyHandler}
     ]
     web_app.add_handlers(host_pattern, handlers)
