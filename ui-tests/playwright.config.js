0ab38ad51a055c263d7c5673ba623f9b0dab2bce
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2026-05-07 08:54:55 -0700

    update tests

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -9,8 +9,8 @@ module.exports = {
     command: 'jlpm start',
     url: 'http://localhost:8888/lab',
     env: {
-      MAAP_API_URL: 'https://api.dit.maap-project.org/api/',
-      MAAP_PGT_TOKEN: 'PGT-my_test_token'
+      MAAP_API_HOST: 'https://api.dit.maap-project.org/api/',
+      MAAP_PGT: 'PGT-my_test_token'
     },
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI
