e12e53348bf365d5aaa38c8347c825f1603e784b
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2026-05-07 09:18:45 -0700

    fix merge conflicts

--- maap_jupyter_server_extension/__init__.py
+++ maap_jupyter_server_extension/__init__.py
@@ -1,26 +1,20 @@
-
-import json
-from pathlib import Path
-
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback when using the package in dev mode without installing
+    # in editable mode with pip. It is highly recommended to install
+    # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
+    import warnings
+    warnings.warn("Importing 'maap_jupyter_server_extension' outside a proper installation.")
+    __version__ = "dev"
 from .handlers import setup_handlers
-
-
-
-HERE = Path(__file__).parent.resolve()
-
-
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
 
 
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": data["name"]
+        "dest": "maap-jupyter-server-extension"
     }]
-
-
 
 
 def _jupyter_server_extension_points():
@@ -38,7 +32,5 @@ def _load_jupyter_server_extension(server_app):
         JupyterLab application instance
     """
     setup_handlers(server_app.web_app)
-    server_app.log.info("Registered HelloWorld extension at URL path /jupyter-server-extension")
-
-# For backward compatibility with notebook server - useful for Binder/JupyterHub
-load_jupyter_server_extension = _load_jupyter_server_extension
+    name = "maap_jupyter_server_extension"
+    server_app.log.info(f"Registered {name} server extension")
